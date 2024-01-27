f4b59f8b64382c955707c962d2d9e6ead359d14b
Carlotta <50404166+carlotta-mah@users.noreply.github.com>
2024-01-27 11:41:20 +0200

    support multiple files

--- src/main.py
+++ src/main.py
@@ -2,14 +2,28 @@
 # Author: Carlotta Mahncke
 import sys
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+import glob
 from sklearn.model_selection import train_test_split
 
 # Step 1: Load the data
-def read_keystroke_data(filename):
+def read_keystroke_data(file_pattern, limit=None):
+    # columns that we want to process
     columns_to_keep = ['PRESS_TIME', 'RELEASE_TIME', 'KEYCODE', 'USER_INPUT']
-    df = pd.read_csv(filename, sep='\t', usecols=columns_to_keep)
-    return df
+
+    # Find files matching the specified pattern
+    file_list = glob.glob("../data/Keystrokes/files/" + file_pattern)
+    file_list = file_list[:limit or len(file_list)]
+
+    # Initialize an empty list to store DataFrames
+    df_list = []
+
+    # Read data from each file and append to the list
+    for file in file_list:
+        df = pd.read_csv(file, sep='\t', usecols=columns_to_keep)
+        df_list.append(df)
+
+    return  pd.concat(df_list, ignore_index=True)
+
 
 # Step 2: Data preprocessing
 def preprocess_data(df):
@@ -38,12 +52,25 @@ def scale_features(df):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <filename>")
+        print("Usage: python script.py <filename> or <number of files>")
         sys.exit(1)
 
-    filename = sys.argv[1]
-    try:
-        df = read_keystroke_data(filename)
+    filespec = sys.argv[1]
+    df = None
+
+    #if filename is number process number files
+    if filespec.isdigit():
+        file_pattern = '????_keystrokes.txt'
+        df = read_keystroke_data(file_pattern, filespec)
+    else:
+        try:
+            df = read_keystroke_data(filespec)
+        except FileNotFoundError:
+            print("File not found:", filespec)
+        except Exception as e:
+            print("An error occurred:", e)
+
+    if df is not None:
         df_preprocessed = preprocess_data(df)
 
         # Step 5: Split the data into training and testing sets
@@ -52,7 +79,3 @@ if __name__ == "__main__":
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         print("Number of samples:", len(df))
         print("Sample format:", df.iloc[0])
-    except FileNotFoundError:
-        print("File not found:", filename)
-    except Exception as e:
-        print("An error occurred:", e)
