6da0469fb03c1036e2c7afe681d68ad0645bf1ad
Carlotta <50404166+carlotta-mah@users.noreply.github.com>
2024-03-08 22:53:20 +0200

    better implementation of data split and softmax

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,22 +1,24 @@
 """ Class implementing a neural network.
- With layers using the sigmoid activation function.
+ With layers using the given activation function.
+ The last layer always uses the softmax activation function.
  We initialize random weights and biases.
  Then it trains the network using backpropagation with gradient descent.
  Finally, it makes predictions using the trained neural network."""
 
 import numpy as np
-from typing import Dict, List, Tuple
-
-import pandas as pd
+from typing import Dict, List
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 
 
 def softmax(x):
-    exps = np.exp(x)
-    return exps / np.sum(exps, axis=1, keepdims=True)
-
+    max_vals = np.max(x, axis=1)
+    # shape needed for broadcasting
+    max_vals = max_vals[:, np.newaxis]
+    e_x = np.exp(x - max_vals)
+    sums = np.sum(e_x, axis=1)
+    # shape needed for broadcasting
+    sums = sums[:, np.newaxis]  # dito
+    return e_x / sums
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -24,16 +26,16 @@ def sigmoid(x):
 
 class NeuralNetwork:
     def __init__(self,
-                 file=None,
+                 file=None, #path to previously saved model
                  layer_structure=None,
                  learning_rate=0.01,
                  activation='relu',
                  validation_split=0.2,
                  ):
         self._batch_size = 32
-        if file is not None:
+        if file is not None: # if file provided it is used
             self.load_model(file)
-        else:
+        else: # if no file provided the other parameters are considered
             if layer_structure is None:
                 raise ValueError("Layer structure is required.")
             self.learning_rate = learning_rate
@@ -45,8 +47,10 @@ class NeuralNetwork:
             self._validation_split = validation_split
 
     def _initialize_weights_and_biases(self):
+        # initialize bias with 1
         self.biases = [np.ones((1, size)) for size in self.layer_sizes[1:]]
-        self.weights = [np.random.randn(self.layer_sizes[i - 1], self.layer_sizes[i]) for i in
+        # random weights
+        self.weights = [np.random.randn(self.layer_sizes[i - 1], self.layer_sizes[i]) for i in #initi
                         range(1, self.layer_count)]
 
     def _activation_function(self, x):
@@ -90,17 +94,21 @@ class NeuralNetwork:
 
     # Backpropagation: figure out how bad hypothesis (each assigned weight) is -> derive E
     def backward(self, activations, error):
-        # compute for output layer
-
-        # init deltas
+        # init deltas with grad from last layer
         deltas = np.multiply(error, (activations[-1]))
+
+        # iterate through layers in reverse order
         for i in reversed(range(self.layer_count - 1)):
+
+            # for the last layer we do not need the derivative, it comes with the error
             if i < self.layer_count - 2:
                 deltas = np.multiply(deltas, self._activation_derivative(activations[i + 1]))
 
+            # calc new weights and biases
             w_pre = activations[i].T @ deltas
             b_pre = np.mean(deltas)
 
+            #deltas for i-1 layer
             deltas = deltas @ self.weights[i].T
 
             self.weights[i] -= self.learning_rate * w_pre
@@ -112,19 +120,18 @@ class NeuralNetwork:
         return predicted - actual
 
     def calculate_mse(self, actual: np.ndarray, predicted: np.ndarray) -> np.ndarray:
-        return (actual - predicted) ** 2
+        return (actual - predicted) ** 2 # mean squared error
 
     def train(self, X, y, epochs):
-        if X.shape[1] != self.layer_sizes[0]:
-            raise ValueError("Number of features in X does not match the size of the input layer.")
+        '''train model for number of epochs: epochs, with data: X and labels: y'''
+        validation_pred = []
         # validation split
         X, X_v, y, y_v = train_test_split(X, y, test_size=self._validation_split, random_state=12)
 
-        # indices = np.random.permutation(len(X))
-        # X_shuffled, y_shuffled = np.array(X[indices]), np.array(y[indices])
+        if X.shape[1] != self.layer_sizes[0]:
+            raise ValueError("Number of features in X does not match the size of the input layer.")
 
         for epoch in range(epochs):
-            # Shuffle the data and divide into mini-batches
             epoch_losses = []
             for i in range(0, len(X), self._batch_size):
                 try:
@@ -135,13 +142,13 @@ class NeuralNetwork:
                     pred, activations = self.forward(x_batch)
                     # Calculate loss
                     batch_loss = self.cross_entropy(y_batch, pred) / len(x_batch)
-                    # batch_loss = self.categorical_cross_entropy(y_batch, pred)
                     epoch_losses.append(batch_loss)
                     # Backward pass
                     self.backward(activations, self.calculate_abs_loss(y_batch, pred))
                 except ValueError as e:
                     raise ValueError("failed in epoch: ", epoch, " with error: ", e)
 
+            # calc total losses for epoch
             train_loss = np.mean(epoch_losses)
             self._losses["train"].append(train_loss)
 
@@ -153,7 +160,13 @@ class NeuralNetwork:
             if epoch % 100 == 0:
                 print(f'Epoch {epoch}, Loss: {train_loss}, Validation Loss: {np.mean(validation_loss)}')
 
+        # print stats of training process
+        print("accuracy: ", self.calculate_accuracy(y_v, validation_pred))
+        print("precision: ", self.calculate_macro_precision(y_v, validation_pred))
+
     def get_prediction(self, X):
+        '''get prediction with current state of model'''
+
         pred, _ = self.forward(X)
         # set the highest probability to 1 and the rest to 0
         m = np.zeros_like(
@@ -162,6 +175,7 @@ class NeuralNetwork:
         return m
 
     def save_model(self, filename):
+        ''' saves model attribures to file'''
         model = {'weights': self.weights, 'biases': self.biases, 'activation': self.activation,
                  'layer_sizes': self.layer_sizes,
                  'losses': self._losses, 'learning_rate': self.learning_rate, 'batch_size': self._batch_size,
@@ -169,6 +183,7 @@ class NeuralNetwork:
         np.save(filename, model)
 
     def load_model(self, filename):
+        # loads model from file
         model = np.load(filename, allow_pickle=True)
         self.layer_sizes = model.item().get('layer_sizes')
         self.layer_count = len(self.layer_sizes)
@@ -190,22 +205,30 @@ class NeuralNetwork:
         return
 
     def calculate_accuracy(self, y_true, predictions):
+        '''caclulates accuracy for multi class prediction'''
         return np.mean(np.argmax(predictions, axis=1) == np.argmax(y_true, axis=1))
 
     def calculate_macro_precision(self, y_true, y_pred):
+        ''' calculate macro precision for multi class classification'''
+
+        # get index of predicted class
         y_true = np.argmax(y_true, axis=1)
         y_pred = np.argmax(y_pred, axis=1)
-        classes = list(np.unique(y_true))
+
+        # init precision
         precision = 0
+
+        #iterate through classes
+        classes = list(np.unique(y_true))
         for c in classes:
-            c_true = np.array([1 if p == c else 0 for p in y_true])
-            c_pred = np.array([1 if p == c else 0 for p in y_pred])
+            c_true = np.array([1 if p == c else 0 for p in y_true]) # true values per class
+            c_pred = np.array([1 if p == c else 0 for p in y_pred]) # predicted values per class
             true_positive = np.sum(np.dot(c_true, c_pred))
             false_positive = np.sum(np.dot((1 - c_true), c_pred))
-            if true_positive + false_positive == 0:
+            if true_positive + false_positive == 0: #if divisor would be 0, precision is 0
                 continue
-            precision += true_positive / (true_positive + false_positive)
-        return precision / len(classes)
+            precision += true_positive / (true_positive + false_positive) #precision per class
+        return precision / len(classes) #mean precsion over all classes
 
     def cross_entropy(self, y_true, y_pred):
         # add epsilon to prevent log(0)
